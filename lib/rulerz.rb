30184e664be32d868d316ecc6663ec9c8de247ea
Nick <nickpassarella@gmail.com>
2017-02-13 21:13:48 -0500

    Add array sum support method.

--- lib/rulerz.rb
+++ lib/rulerz.rb
@@ -1,8 +1,10 @@
-require "rulerz/version"
+require 'rulerz/version'
+require 'rulerz/array'
 
 module Rulerz
   class Application
     def call(env)
+      `echo debug > debug.txt`;
       [200, {'Content-Type' => 'text/html'},
          ['Hello from Ruby on Rulerz!']]
     end
